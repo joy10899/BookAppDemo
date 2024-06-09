6a22aa2167c2f20d4e69ae7e2af681f7c930e129
Joy <joytran10899@gmail.com>
2024-06-09 13:29:48 -0700

    Initial Commit

---
+++ BookAppDemo/BookAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppDemoApp.swift
+//  BookAppDemo
+//
+//  Created by Joy on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BookAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
